6deec0a9afd967f9cc41efb65b94ef3dccc3ada6
[Huangtianhui] <[huihuijiang0619@gmail.com]>
2016-12-11 00:12:39 -0500

    update javascript

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,26 +1,40 @@
-
+/*
+ * ui object encloses all UI related methods and attributes
+ */
 var ui = {};
 
+//holds the state of the intial controls visibility
 ui.intialControlsVisible = true;
 
+//holds the setInterval handle for the robot flickering
 ui.robotFlickeringHandle = 0;
 
+//holds the current visible view
 ui.currentView = "";
 
+/*
+ * starts the flickering effect of the robot image
+ */
 ui.startRobotFlickering = function() {
     ui.robotFlickeringHandle = setInterval(function() {
         $("#robot").toggleClass('robot');
     }, 500);
 };
 
-
+/*
+ * stops the flickering effect on the robot image
+ */
 ui.stopRobotFlickering = function() {
     clearInterval(ui.robotFlickeringHandle);
 };
 
-
+/*
+ * switchs the view on the UI depending on who's turn it switchs
+ * @param turn [String]: the player to switch the view to
+ */
 ui.switchViewTo = function(turn) {
 
+    //helper function for async calling
     function _switch(_turn) {
         ui.currentView = "#" + _turn;
         $(ui.currentView).fadeIn("fast");
@@ -30,6 +44,7 @@ ui.switchViewTo = function(turn) {
     }
 
     if(ui.intialControlsVisible) {
+        //if the game is just starting
         ui.intialControlsVisible = false;
 
         $('.intial').fadeOut({
@@ -40,6 +55,7 @@ ui.switchViewTo = function(turn) {
         });
     }
     else {
+        //if the game is in an intermediate state
         $(ui.currentView).fadeOut({
             duration: "fast",
             done: function() {
@@ -49,6 +65,12 @@ ui.switchViewTo = function(turn) {
     }
 };
 
+/*
+ * places X or O in the specifed place in the board
+ * @param i [Number] : row number (0-indexed)
+ * @param j [Number] : column number (0-indexed)
+ * @param symbol [String]: X or O
+ */
 ui.insertAt = function(indx, symbol) {
     var board = $('.cell');
     var targetCell = $(board[indx]);
